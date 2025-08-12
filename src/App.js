72586473175120670f3ac44a66ad05867b1ccb26
arosha112432 <ar112432@fhss.sjp.ac.lk>
2025-08-12 10:14:32 +0530

    implement admin side nav bar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hompage from './Components/Hompage';
 import Footer from './Components/Footer';
 
+import AdminNav from './Components/Admin/AdminNav'; // Import the sidebar nav
+
+
 function App() {
   return (
     <Router>
       <div className="app-container">
         <Routes>
           <Route path="/" element={<Hompage />} />
+          <Route path="/admin" element={<AdminNav />} />
           {/* Add more routes here if needed */}
         </Routes>
       <Footer/>{}
